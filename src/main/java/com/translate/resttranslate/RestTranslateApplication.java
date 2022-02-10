43926cf503897b7296d299e029a29a8e30e30565
unknown <nikhilgupta2581@gmail.com>
2022-02-10 13:27:34 +0530

    'initial'

---
+++ src/main/java/com/translate/resttranslate/RestTranslateApplication.java
@@ -0,0 +1,13 @@
+package com.translate.resttranslate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTranslateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTranslateApplication.class, args);
+	}
+
+}
